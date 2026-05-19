10738db2bd903d8e6d15ead4fb48bcca9bbba31f
Matheow2 <matius152014@gmail.com>
2026-05-19 18:02:30 -0500

    config: Iniciando proyecto

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import http from 'http';
+
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    res.write("Hello World");
+    res.end();
+});
+
+server.listen(8080, ()=>{
+    console.log('Server is running on port 8080');
+});
