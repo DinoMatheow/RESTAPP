023e7380f2cc78e8ebd3ffdaee330bdb00a6af17
Matheow2 <matius152014@gmail.com>
2026-05-20 17:33:10 -0500

    feat: creacion de public/

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import http from 'http';
+import fs from 'fs';
 
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.write("Hello World");
-    res.end();
+
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write( `URL  ${req.url}) `);
+    // res.end();
+    // const data = {
+    //     name: 'John Doe',
+    //     age: 30,
+    //     city: 'New York',
+    // };
+
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.end(JSON.stringify(data));
+    if( req.url === '/'){
+        const htmlFile = fs.readFileSync("./public/index.html", "utf8");
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(htmlFile);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'}); 
+        res.end();
+    }
+
 });
 
 server.listen(8080, ()=>{
