c583dda9a55d4f7ad709b3ba9de695f86bb8e51f
Matheow2 <matius152014@gmail.com>
2026-05-23 17:57:08 -0500

    config: configuracion de entorno http2

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import http from 'http';
 import fs from 'fs';
+import http2 from 'http2';
 
 
-const server = http.createServer((req, res) => {
+const server = http2.createSecureServer({
+    key: fs.readFileSync("./keys/server.key"),
+    cert: fs.readFileSync("./keys/server.crt"),
+}, (req, res) => {
     console.log(req.url);
 
     // res.writeHead(200, {'Content-Type': 'text/html'});
@@ -20,10 +24,26 @@ const server = http.createServer((req, res) => {
         const htmlFile = fs.readFileSync("./public/index.html", "utf8");
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(htmlFile);
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'}); 
-        res.end();
+
+        return;
+    } 
+
+
+
+    if (req.url?.endsWith(".js")){
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
+
+    }else if(req.url?.endsWith(".css")){
+        res.writeHead(200, {'Content-Type': 'text/css'});
     }
+
+    const responseContent = fs.readFileSync(`./public${req.url}`, "utf8");
+    res.end(responseContent);
+
+
+
+
+
 
 });
 
