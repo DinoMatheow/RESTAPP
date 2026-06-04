ed05bf3aed49c969e853c09b0008b1c31f1482b3
Matheow2 <matius152014@gmail.com>
2026-06-04 17:46:34 -0500

    feat: Implementacion de createTodo tipo POST

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -9,6 +9,8 @@ export class TodoRoutes{
         const todosController = new TodosController();
         router.get("/", todosController.getTodos);
         router.get("/:id", todosController.getTodoById);
+        router.post("/", todosController.createTodo);
+
         
 
         return router;
